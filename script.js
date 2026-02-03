7bd7756f51fedf0ff29c88c0916b2cdb03e91beb
kevinevason <kevinevason@hotmail.com>
2026-02-03 15:53:34 +0100

    Updated all scripts.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const moonUrl = "https://github.com/kevinevason/kevinevason_website/blob/main/moon.png?raw=true";
 
     function updateUI() {
-        const isDark = body.classList.contains("dark-mode");//hejewre
+        const isDark = body.classList.contains("dark-mode");//hejewreasdf
         
         // If dark mode is on, show moon. Otherwise, show sun.
         themeIcon.src = isDark ? moonUrl : sunUrl;
